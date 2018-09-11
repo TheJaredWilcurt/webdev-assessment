e4ebaf3b576040c6e6e6c90544985570a54db5ce
Stephan Raab <sarcartist@gmail.com>
2018-09-11 07:11:02 -0400

    Remove lingering comments

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,5 @@ app.use(bodyParser.json())
 app.use(catsRouter)
 app.use(express.static('public'))
 
-// app.use((req,res,next) => {
-
-// })
-
 const port = process.env.port || 8080
 app.listen(port, () => console.info(`Server running on localhost:${port}`))
