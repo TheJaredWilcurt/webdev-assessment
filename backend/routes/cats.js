b93d9a9d2cc65f2f082b31e4f5679acab115eb36
Pop!_os <pop_os@pop-os.localdomain>
2018-09-10 21:53:00 -0400

    get 404 ERROR

--- backend/routes/cats.js
+++ backend/routes/cats.js
@@ -34,29 +34,24 @@ router.get('/cats', (req, res) => {
 })
 
 // GET to /cats/:id returns the same object created from the first POST request when called with the generated id
-router.get('/cats/:id', (req, res) => {
-    let id = req.params.id -1
-    res.json(catsObject.catsList[id])
-})
 // GET to /cats/:id returns a 404 status code for a cat that does not exist
 router.get('/cats/:id', (req, res) => {
-    res.status(404).send('404')
-    console.log('404 hit')
-    // if (catsObject.catsList.includes(req.params.id)) {
-    //     // res.status(404).send('404');
-    //     console.log(req.params, 'not found in list')
-    // } else {
-    //     console.log(req.params, 'FOUND!')
-    // }
+    let id = req.params.id -1
+    array = catsObject.catsList.map(m => m.id)
+    if (!array.includes(parseInt(id)))
+        res.status(404).send('404')
+        
+    res.json(catsObject.catsList[id])
 })
 
 // PUT to /cats/:id updates the record identified by :id
-router.put('/cats/:id/:name', (req, res) => {
+router.put('/cats/:id/', (req, res) => {
     let id = req.params.id -1
     let name = req.params.name
     let newName = catsObject.catsList[id].name = name
     res.json(newName)
 })
+
 // PUT to /cats/:id with an invalid id responds with a 404 status code
 // DELETE to /cats/:id removes the record
 // DELETE to /cats/:id with an invalid id responds with a 404 status code
